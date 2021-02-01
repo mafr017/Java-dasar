7599c02f05c59565a63e4b61db338da9658e956c
mafr017 <madityafr3@gmail.com>
2021-02-01 19:54:25 +0700

    Membuat tipe data character

--- README.md
+++ README.md
@@ -4,5 +4,6 @@
 2. Tipe Data
 3. Literals
 4. Konversi Tipe Data
+5. Tipe Data Character
 
 Referensi:  [Programmer Zaman Now](https://www.youtube.com/ProgrammerZamanNow)
---
+++ src/TipeDataCharacter.java
@@ -0,0 +1,23 @@
+public class TipeDataCharacter {
+    public static void main(String[] args) {
+        /**
+         * char menggunakan (' ')
+         * string menggunakan (" ")
+         */
+        char a = 'A';
+        char d = 'd';
+        char i = 'i';
+        char t = 't';
+        char y = 'y';
+
+        String aditya = "Aditya";
+
+        System.out.print(a);
+        System.out.print(d);
+        System.out.print(i);
+        System.out.print(t);
+        System.out.print(y);
+        System.out.println(a);
+        System.out.println(aditya);
+    }
+}
